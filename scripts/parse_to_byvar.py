b02c780e9a9c612687ea6475be8aaad47a5de625
martinjuckes <Martin.Juckes@stfc.ac.uk>
2020-05-08 19:29:05 +0100

    adding parse to byvar

---
+++ scripts/parse_to_byvar.py
@@ -0,0 +1,22 @@
+import collections, os, glob
+
+def p2var():
+    fl = glob.glob( "inputs/lsout/files_x1*.txt"  )
+    cc = collections.defaultdict( set )
+    for f in fl:
+      ii = open( f ).readlines()
+      for l in ii:
+        parts = l.strip().split( "/"  )
+        tab, var = parts[ 10:12 ]
+        cc[ (tab,var) ].add( l.strip() )
+    for k in cc.keys():
+      oo = open( "inputs/byvar/x1_%s_%s_latest.txt" % k, "w" )
+      for l in sorted( list( cc[k] ) ):
+        oo.write( l + "\n" )
+      oo.close()
+ 
+
+
+
+p2var()
+print( "DONE" )
