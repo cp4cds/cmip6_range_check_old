30b97b7de9ab4c279946e4859ad6c489f32386ce
martinjuckes <Martin.Juckes@stfc.ac.uk>
2020-05-11 13:16:44 +0100

    adding utils and parsex1

---
+++ scripts/utils.py
@@ -0,0 +1,8 @@
+
+
+
+##
+## 23 tables
+##
+ntable = 23
+table_list = "Oyr Omon AERmon Amon CFmon LImon SImon Emon EmonZ ImonGre AERmonZ ImonAnt day Oday CFday Eday EdayZ 3hr E3hr CF3hr 6hrPlev Ofx fx".split()
