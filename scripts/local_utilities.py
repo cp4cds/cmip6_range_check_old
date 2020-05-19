5bd1cceb010b73d6a3ada9fcc27741c7a957f6c1
Martin Juckes <martin.juckes@stfc.ac.uk>
2020-05-19 06:35:47 +0100

    local utilities update

--- scripts/local_utilities.py
+++ scripts/local_utilities.py
@@ -1,4 +1,4 @@
-import logging, time, os, collections
+import logging, time, os, collections, json, inspect
 
 NT_RangeValue = collections.namedtuple( "range_value", ["value","status"] )
 NT_RangeSet = collections.namedtuple( "range_set", ["max","min","ma_max","ma_min"] )
@@ -23,6 +23,101 @@ class WGIPriority(object):
         self.ranges[id] = NT_RangeSet( xx[0], xx[1], xx[2], xx[3] )
 
       self.ee[id] = units
+
+class CheckJson(object):
+  def __init__(self):
+    self.new = json.load( open( "data/new_limits.json", "r" ) )
+    self.new_modified = set()
+
+  def get_range(self,varid):
+    xx = []
+    this = self.new["data"][varid]["ranges"]
+    for k in ["max","min","ma_max","ma_min"]:
+      if k in this:
+        xx.append( NT_RangeValue( this[k][0], this[k][1] ) )
+      else:
+        xx.append( null_range_value )
+    return NT_RangeSet( xx[0], xx[1], xx[2], xx[3] )
+
+  def set_range(self,varid, max=None, min=None, ma_max=None, ma_min=None ):
+    """set a new range value or values in local instance"""
+    self.sig = inspect.signature( self.set_range )
+    ee = dict()
+    args = list( self.sig.parameters.keys() )
+    for k in args[1:]:
+      val = locals()[k]
+      if val != None:
+        if type(val) == type( () ):
+          ee[k] = val
+        elif type(val) in [type( 1. ),type( 1 )]:
+          ee[k] = (val,"provisional")
+        else:
+          print ("value for arg %s not recognised" % k )
+          raise
+
+    if varid in self.new["data"]:
+      this = self.new["data"][varid]
+      this["ranges"]
+      for k in prev:
+        if k not in ee:
+          ee[k] = prev[k]
+      this["ranges"] = ee
+      hist = this["history"]
+      hist.append( "Record updates %s" % time.ctime() )
+      this["history"] = this
+    else:
+      this = {"ranges":ee, "history":"Record created %s" % time.ctime() }
+
+    self.new["data"][varid] = this
+    if len( ee.keys() ) > 0:
+      self.new_modified.add( varid )
+     
+   
+  def __call__(self, table,ipath=None,var=None):
+    if ipath == None:
+      assert var !=None, "check_json: either ipath or var must be set"
+      ipath = "json_ranges/%s/%s_historical_consol-var.json" % (table,var)
+
+    assert os.path.isfile( ipath ), "check_json: file %s not found" % ipath
+
+    ifile = ipath.rpartition("/")[-1]
+    var = ifile.split("_")[0]
+    wg1 =  WGIPriority()
+    varid = "%s.%s" % (table,var)
+    print( "check_json",table, ipath, varid )
+    if varid not in wg1.ranges and varid not in self.new["data"]:
+      print ( "No range information for %s" % varid )
+      return None
+    else:
+      if varid in self.new["data"]:
+        ranges = self.get_range( varid )
+      else:
+        ranges = wg1.ranges[varid]
+      ee = json.load( open( ipath, "r" ) )
+      data = ee["data"]
+      rsum = dict()
+      for m in sorted( list( data.keys() ) ):
+        this = data[m]["summary"]
+        range_error_max = this[1] > float(ranges.max.value)
+        range_error_min = this[2] < float(ranges.min.value)
+        if not any( [range_error_max,range_error_min] ):
+           res = (True,"OK")
+        elif all( [range_error_max,range_error_min] ):
+           res = (False, "ERROR: Max and Min range errors: %s > %s and %s < %s" % (this[1],ranges.max.value,this[2],ranges.min.value) )
+        elif range_error_max:
+           res = (False, "ERROR: Max range error: %s > %s" % (this[1],ranges.max.value) )
+        elif range_error_min:
+           res = (False, "ERROR: Min range error: %s < %s" % ( this[2],ranges.min.value) )
+        print ("%s:: %s" % (m,res[1]) )
+        rsum[m] = res[0]
+
+    bad = [k for k,v in rsum.items() if not v]
+    if len( bad) == 0:
+       print ("All models in range")
+    else:
+       print( "WARNING: %s models (from %s) out of range" % (len(bad),len(rsum.keys())) )
+
+check_json = CheckJson()
 
 class LogFactory(object):
   def __init__(self, dir='.'):
